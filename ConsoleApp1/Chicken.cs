1a75548a0a763be6376a096bc65c1abefb8ce2ed
Jacob Grimm <grimmj2@erau.edu>
2018-09-07 09:50:04 -0400

    adds chicken produce

--- ConsoleApp1/Chicken.cs
+++ ConsoleApp1/Chicken.cs
@@ -14,5 +14,12 @@ namespace ConsoleApp1
                 "what I say you should see me in the squared circle brother, I'm a force to be\n" +
                 "reconned with\n");
         }
+        public void Produce()
+        {
+            Console.WriteLine("As a chicken I produce many things, things such as eggs\n" +
+                "everyone likes eating my eggs. Chicken nuggets? You like those don't you\n" +
+                "well that's me again. Basically any food that has chicken in the title\n" +
+                "comes from us chickens.");
+        }
     }
 }
