9c1d9fa4c712167dac8bf49fdd8990cd3df8ca90
Jacob Grimm <grimmj2@erau.edu>
2018-09-07 10:19:38 -0400

    adds pig eat

--- ConsoleApp1/PIg.cs
+++ ConsoleApp1/PIg.cs
@@ -13,9 +13,14 @@ namespace ConsoleApp1
             Console.WriteLine("I am a pig and I say 'oink' but I also say things like 'what a pig!' well ok actually\n" +
                 "thats my friend the spider.\n");
         }
-        public void produce()
+        public void Produce()
         {
             Console.WriteLine("Pigs like me produce lots but all anyone ever wants to talk about is the bacon :(");
+        }
+        public void Eat()
+        {
+            Console.WriteLine("People often ask me, they say 'hey pig! What do you want to eat?' Well let me tell\n" +
+                "you that is the beauty of being a pig. I eat everything!");
         }
     }
 }
