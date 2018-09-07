060ef2f568c775919db036d76bd0bc3053c3d708
Jacob Grimm <grimmj2@erau.edu>
2018-09-07 09:38:30 -0400

    adds extra methods to the cow class

--- ConsoleApp1/Cow.cs
+++ ConsoleApp1/Cow.cs
@@ -13,5 +13,18 @@ namespace ConsoleApp1
             Console.WriteLine($"I am a cow and sometimes I say 'moo' but sometimes I wish I \n" +
                 $"could say something to farmer Ted about his roaming hands...\n");
         }
+        public void Eat()
+        {
+            Console.WriteLine("Yum! I love eating grass, but sometimes I like to eat other\n" +
+                "cows. Who wants mad cow disease?!");
+        }
+        public void Produce()
+        {
+            Console.WriteLine("I make milk, and that milk makes cheese so that's pretty great.");
+        }
+        public void move()
+        {
+            Console.WriteLine("As a cow I just mosey around, sometimes I move in a herd.");
+        }
     }
 }
