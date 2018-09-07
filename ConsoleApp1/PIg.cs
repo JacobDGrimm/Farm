d97c35f769e1eb8f8731253ddc9497d140814a7b
Jacob Grimm <grimmj2@erau.edu>
2018-09-07 10:16:34 -0400

    adds pig produce

--- ConsoleApp1/PIg.cs
+++ ConsoleApp1/PIg.cs
@@ -13,5 +13,9 @@ namespace ConsoleApp1
             Console.WriteLine("I am a pig and I say 'oink' but I also say things like 'what a pig!' well ok actually\n" +
                 "thats my friend the spider.\n");
         }
+        public void produce()
+        {
+            Console.WriteLine("Pigs like me produce lots but all anyone ever wants to talk about is the bacon :(");
+        }
     }
 }
